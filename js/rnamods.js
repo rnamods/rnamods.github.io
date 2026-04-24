7abe9aa56d3352be8f8142b92aa21192da4e3f6e
RNAmods <rnamods@gmail.com>
2026-04-25 01:35:18 +0200

    Update rnamods.js

--- js/rnamods.js
+++ js/rnamods.js
@@ -93,7 +93,7 @@ thetxt = thetxt.replace(/\!b/g,"<SPAN class='mygreek'>&beta;</SPAN>");
 thetxt = thetxt.replace(/\!g/g,"<SPAN class='mygreek'>&gamma;</SPAN>");
 thetxt = thetxt.replace(/\!d/g,"<SPAN class='mygreek'>&delta;</SPAN>");
 thetxt = thetxt.replace(/\!t/g,"<SPAN class='mygreek'>&tau;</SPAN>");
-thetxt = thetxt.replace(/doi\:([\w\/\.\_\-\;\(\)]+)([\s\n])/g,"<A HREF='https://doi.org/$1'>doi:$1</A>$2");	
+thetxt = thetxt.replace(/\[doi\:(.+?)\]/g,"[<A HREF='https://doi.org/$1'>doi:$1</A>]");	
 return thetxt;
 }
 //------------------------------------------
